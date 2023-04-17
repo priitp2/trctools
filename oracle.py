1848e9836bcb9c3362d2ed3d292b658b4a247a2f
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-04-17 14:16:56 +0000

    Dump cursors and execs in the db

--- oracle.py
+++ oracle.py
@@ -16,3 +16,13 @@ class DB:
             cursor.execute("insert into rtime (sql_id, rt) values(:1, :2)", [sql_id, r])
 
         self.connection.commit()
+    def add_cursor(self, c):
+        cursor = self.connection.cursor()
+        cursor.execute("insert into cursors(cursor_id, statement_length, rec_depth, schema_id, command_type, priv_user_id, ts, hash_id, sqltext_addr, sql_id) values(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10)", [c.cursor, c.statement_length, c.rec_depth, c.schema_uid, c.command_type, c.priv_user_id, c.timestamp, c.hash_id, c.address, c.sql_id])
+        self.connection.commit()
+    def add_event(self, ev):
+        cursor = self.connection.cursor()
+        out = cursor.var(int)
+        cursor.execute("insert into events(parent_id, event, cursor_id, cpu_time, elapsed_time, ph_reads, cr_reads, current_reads, cursor_missed, rows_processed, rec_call_dp, opt_goal, ts, c_type) values(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14) returning id into :15", [ev['parent_id'], ev['event'], ev['cursor'], ev['c'], ev['e'], ev['p'], ev['cr'], ev['cu'], ev['mis'], ev['r'], ev['dep'], ev['og'], ev['tim'], ev['type'], out])
+        self.connection.commit()
+        return out.getvalue()
