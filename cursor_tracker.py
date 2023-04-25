955a0521c0a5c18fc96cb42362301157426253cc
Priit Piipuu <priit.piipuu@gmail.com>
2023-04-25 06:18:10 +0000

    PARSE calls might not be in the trace

--- cursor_tracker.py
+++ cursor_tracker.py
@@ -21,7 +21,12 @@ class CursorTracker:
             overwrite the latest_cursor.
         '''
         if cursor in self.latest_cursors.keys():
-            statement = self.statements[self.cursors[cursor]]
+            # FIXME: Trace can contain cursor without matching statement
+            if cursor in self.cursors.keys():
+                statement = self.statements[self.cursors[cursor]]
+            else:
+                print("add_latest_cursor: unknown cursor {}, probably missing PARSE* operation".format(cursor))
+                return None
             cs = self.latest_cursors[cursor]
             statement.add_current_statement(cs)
         self.latest_cursors[cursor] = CurrentStatement(cursor)
@@ -67,7 +72,11 @@ class CursorTracker:
         cs = self.latest_cursors[cursor]
         cs.add_wait(params)
     def add_close(self, cursor, params):
-        cs = self.latest_cursors[cursor]
+        try:
+            cs = self.latest_cursors[cursor]
+        except KeyError:
+            print("add_close: stray cursor {}".format(cursor))
+            return None
         cs.add_close(params)
         self.add_latest_cursor(cursor)
         return cs
