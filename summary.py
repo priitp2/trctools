5bce407dc32edbf6d905a5b22a450f303987bea4
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-06-09 12:16:26 +0000

    Improve readability

--- summary.py
+++ summary.py
@@ -5,10 +5,27 @@ from hdrh.histogram import HdrHistogram
 class SummaryDuckdb:
     def __init__(self, dbdir):
         self.dbdir = dbdir
-        d.sql("create or replace view elapsed_time as select sql_id, exec_id, last( ts order by ts) - first(ts order by ts) as ela from read_parquet('{}') where ts is not null group by sql_id, exec_id;".format(dbdir))
+        d.sql("""create or replace view elapsed_time as
+                select sql_id,
+                    exec_id,
+                    last( ts order by ts) - first(ts order by ts) as ela
+                from
+                    read_parquet('{}')
+                where
+                    ts is not null
+                group by sql_id, exec_id;
+              """.format(dbdir))
 
     def summary(self):
-        res = d.sql("select sql_id, count(*) execs, median(ela), percentile_disc(0.99) within group(order by ela) from elapsed_time group by sql_id order by execs").fetchall()
+        res = d.sql("""select sql_id,
+                            count(*) execs,
+                            median(ela),
+                            percentile_disc(0.99) within group(order by ela)
+                        from
+                            elapsed_time
+                        group by sql_id
+                        order by execs
+                    """).fetchall()
         for r in res:
             print("sq_id = {}, execs = {}, median = {}, 99th percentile = {}".format(r[0], r[1], r[2], r[3]))
 
@@ -20,15 +37,45 @@ class SummaryDuckdb:
         with open("elapsed_{}.out".format(sql_id), 'wb') as f:
             resp_hist.output_percentile_distribution(f, 1.0)
     def outliers(self, sql_id, thresold):
-        res = d.sql("""select cursor_id "cursor", exec_id, ops, elapsed_time, ifnull(rows_processed, -1) "rows", ts, wait_name, file_name, line from read_parquet('{}') where sql_id = '{}' and exec_id in (select exec_id from elapsed_time where sql_id = '{}' and ela > {}) order by exec_id, ts""".format(self.dbdir, sql_id, sql_id, thresold))
+        res = d.sql("""select cursor_id "cursor",
+                            exec_id,
+                            ops,
+                            elapsed_time,
+                            ifnull(rows_processed, -1) "rows",
+                            ts,
+                            wait_name,
+                            file_name,
+                            line
+                        from
+                            read_parquet('{}')
+                        where
+                            sql_id = '{}'
+                            and exec_id in (select exec_id from elapsed_time where sql_id = '{}' and ela > {})
+                        order by exec_id, ts
+                    """.format(self.dbdir, sql_id, sql_id, thresold))
         print(res)
     def waits(self, sql_id):
-        res = d.sql("""select wait_name wait, count(*) count, sum(elapsed_time) sum, median(elapsed_time) median, percentile_disc(0.99) within group(order by elapsed_time) "99th percentile" from read_parquet('{}') where sql_id = '{}' and wait_name <> '' group by wait_name order by count(*) desc""".format(self.dbdir, sql_id))
+        res = d.sql("""select wait_name wait,
+                            count(*) count,
+                            sum(elapsed_time) sum,
+                            median(elapsed_time) median,
+                            percentile_disc(0.99) within group(order by elapsed_time) "99th percentile"
+                        from read_parquet('{}')
+                        where sql_id = '{}'
+                            and wait_name <> ''
+                        group by wait_name order by count(*) desc
+                    """.format(self.dbdir, sql_id))
         print(res)
 #        for r in res:
 #            print(r)
     def wait_histogram(self, wait_name, fname):
-        res = d.sql("""select elapsed_time from read_parquet('{}') where ops = 'WAIT' and wait_name = '{}'""".format(self.dbdir, wait_name)).fetchall()
+        res = d.sql("""select elapsed_time
+                        from
+                            read_parquet('{}')
+                        where
+                            ops = 'WAIT'
+                            and wait_name = '{}'
+                    """.format(self.dbdir, wait_name)).fetchall()
         resp_hist = HdrHistogram(1, 1000000000, 1)
         for w in res:
             resp_hist.record_value(w[0])
