6b5c328c6701c33aa2509a069e4afd38760a76fd
Priit Piipuu <priit.piipuu@gmail.com>
2023-06-10 15:19:06 +0000

    Do not generate histograms, this went into summary.py

--- tracer2.py
+++ tracer2.py
@@ -21,8 +21,6 @@ parser.add_argument('--norm', type=bool, default = False, dest='norm',
                             help="Perform Shapiro-Wilk normality test on values")
 parser.add_argument('--db', type=str, default = None, dest='db', help="Persists raw data in the db, supported implementations: oracle, parquet")
 parser.add_argument('--dbdir', type=str, default = 'arrow', dest='dbdir', help="Directory for the parquet files")
-parser.add_argument('--merge_all', type=bool, default = False, dest='merge_all',
-                            help="Merges all sql statements into one histogram. Helpful without the bind variables")
 parser.add_argument('--logfile', type=str, default = None, dest='logfile', help="Sends output to the file")
 parser.add_argument('--log-level', type=str, default = None, dest='loglevel', help="Sets logging level: from DEBUG to CRITICAL")
 
@@ -64,46 +62,4 @@ for fname in args.trace_files:
     fcount += 1
     p = PurePath(fname)
     tracker.flush(p.stem)
-
-if len(ids) == 0:
-    ids = [tracker.statements[s].sql_id for s in tracker.statements.keys()]
-
-if args.merge_all:
-    exec_hist_elapsed = HdrHistogram(1, 1000000000, 1)
-    exec_hist_cpu = HdrHistogram(1, 1000000000, 1)
-    fetch_hist_elapsed = HdrHistogram(1, 1000000000, 1)
-    fetch_hist_cpu = HdrHistogram(1, 1000000000, 1)
-    response_hist = HdrHistogram(1, 1000000000, 1)
-
-    for c in tracker.statements.keys():
-        stat = tracker.statements[c]
-        exec_hist_elapsed.add(stat.exec_hist_elapsed)
-        exec_hist_cpu.add(stat.exec_hist_cpu)
-        response_hist.add(stat.resp_hist)
-    with open("exec_hist_elapsed_all.out", 'wb') as f:
-        exec_hist_elapsed.output_percentile_distribution(f, 1.0)
-    with open("exec_hist_cpu_all.out", 'wb') as f:
-        exec_hist_cpu.output_percentile_distribution(f, 1.0)
-    with open("response_hist_all.out", 'wb') as f:
-        response_hist.output_percentile_distribution(f, 1.0)
-    sys.exit(0)
-
-for c in tracker.statements.keys():
-    stat = tracker.statements[c]
-    if stat.sql_id in ids:
-        print('----------------------------------------')
-        print("sql_id: {}, execs: {}, fetches: {}".format(stat.sql_id, stat.execs, stat.fetches))
-        if args.norm:
-            print("Normality test(Shapiro-Wilkes) on cpu: {}".format(shapiro(stat.exec_cpu)))
-            print("Normality test(Shapiro-Wilkes) on elapsed: {}".format(shapiro(stat.fetch_elapsed)))
-            print("Normality test(Kolmogorov-Smirnov) on cpu: {}".format(kstest(stat.exec_cpu, 'norm')))
-            print("Normality test(Kolmogorov-Smirnov) on elapsed: {}".format(kstest(stat.fetch_elapsed, 'norm')))
-        with open("exec_hist_elapsed_{}.out".format(stat.sql_id), 'wb') as f:
-            stat.exec_hist_elapsed.output_percentile_distribution(f, 1.0)
-        with open("exec_hist_cpu_{}.out".format(stat.sql_id), 'wb') as f:
-            stat.exec_hist_cpu.output_percentile_distribution(f, 1.0)
-        with open("response_hist_{}.out".format(stat.sql_id), 'wb') as f:
-            stat.resp_hist.output_percentile_distribution(f, 1.0)
-        with open("response_hist_without_waits_{}.out".format(stat.sql_id), 'wb') as f:
-            stat.resp_without_waits_hist.output_percentile_distribution(f, 1.0)
 
