5359e0d491c7590e6adc6d8e87588804d523f657
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-06-11 10:25:33 +0000

    Remove print_naughty_exec

--- util.py
+++ util.py
@@ -1,36 +1,6 @@
 import re
 from ops import Ops
 
-def print_naughty_exec(tracker, cs, fname, line, trigger):
-    lat = cs.merge()
-    if lat.e > 1000000:
-        print('----------------------------------------------')
-        if lat.cursor not in tracker.cursors.keys():
-            print("print_naughty_exec: missing cursor {}".format(lat.cursor))
-            return
-        statement = tracker.statements[tracker.cursors[lat.cursor]]
-        print("sql_id = {}, cursor = {}, elapsed = {}, fetches = {}, file = {}, triggered by {}".format(statement.sql_id, lat.cursor, lat.e, cs.fetch_count, fname, trigger))
-        if cs.parse:
-            print("    {}".format(cs.parse))
-        if cs.exec:
-            print("    {}".format(cs.exec))
-        for i in range(0, min(10, len(cs.fetches))):
-            print("     {}".format(cs.fetches[i]))
-        if len(cs.fetches) > 10:
-            print("     <Rest of the {} fetches>".format(len(cs.fetches)))
-        for i in range(0, min(10, len(cs.waits))):
-            print("     {}".format(cs.waits[i]))
-        if len(cs.waits) > 10:
-            print("     <Rest of the {} waits>".format(len(cs.waits)))
-        if cs.close:
-            print("    {}".format(cs.close))
-        elapsed = cs.get_elapsed()
-        if elapsed != None:
-            print("    estimated elapsed time = {}".format(elapsed))
-
-        print('----------------------------------------------')
-
-
 def process_file(tr, fname, sql_ids):
 
     line_count = 0
