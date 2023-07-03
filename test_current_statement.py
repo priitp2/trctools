46c525bbd6961818889cf667d3d58558126bf327
Priit Piipuu <priit.piipuu@gmail.com>
2023-07-03 20:37:18 +0000

    Deploy linter

--- test_current_statement.py
+++ test_current_statement.py
@@ -2,106 +2,106 @@ import unittest
 from current_statement import CurrentStatement
 from ops import Ops
 
-cursor = '#123'
-wrong_cursor = '#321'
-fname = 'trace.trc'
+CURSOR = '#123'
+WRONG_CURSOR = '#321'
+FNAME = 'trace.trc'
 class TestCurrentStatement(unittest.TestCase):
     def test_add_parse(self):
-        cs = CurrentStatement(cursor, None)
-        o = Ops('PARSE', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 0)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('PARSE', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 0)
 
-        cs.add_parse(o)
+        cs.add_parse(ops)
         with self.assertRaisesRegex(BaseException, 'add_parse: already set!'):
-            cs.add_parse(o)
+            cs.add_parse(ops)
 
-        cs = CurrentStatement(cursor, None)
-        o = Ops('EXEC', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 2)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('EXEC', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 2)
         with self.assertRaisesRegex(BaseException, 'add_parse: wrong op_type *'):
-            cs.add_parse(o)
+            cs.add_parse(ops)
 
-        o = Ops('PARSE', wrong_cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 9)
+        ops = Ops('PARSE', WRONG_CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 9)
         with self.assertRaisesRegex(BaseException, 'add_parse: got cursor *'):
-            cs.add_parse(o)
+            cs.add_parse(ops)
 
     def test_add_exec(self):
-        cs = CurrentStatement(cursor, None)
-        o = Ops('EXEC', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 9)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('EXEC', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 9)
 
-        cs.add_exec(o)
+        cs.add_exec(ops)
         with self.assertRaisesRegex(BaseException, 'add_exec: already set!'):
-            cs.add_exec(o)
+            cs.add_exec(ops)
 
-        cs = CurrentStatement(cursor, None)
-        o = Ops('PARSE', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 8)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('PARSE', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 8)
         with self.assertRaisesRegex(BaseException, 'add_exec: wrong op_type *'):
-            cs.add_exec(o)
+            cs.add_exec(ops)
 
-        o = Ops('EXEC', wrong_cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 9)
+        ops = Ops('EXEC', WRONG_CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 9)
         with self.assertRaisesRegex(BaseException, 'add_exec: got cursor *'):
-            cs.add_exec(o)
+            cs.add_exec(ops)
 
     def test_add_wait(self):
-        cs = CurrentStatement(cursor, None)
-        o = Ops('PARSE', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 9)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('PARSE', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 9)
         with self.assertRaisesRegex(BaseException, 'add_wait: wrong op_type *'):
-            cs.add_wait(o)
+            cs.add_wait(ops)
 
-        o = Ops('WAIT', wrong_cursor, " nam='db file sequential read' ela= 403 file#=414 block#=2682927 blocks=1 obj#=89440 tim=5793512314261", fname, 3)
+        ops = Ops('WAIT', WRONG_CURSOR, " nam='db file sequential read' ela= 403 file#=414 block#=2682927 blocks=1 obj#=89440 tim=5793512314261", FNAME, 3)
         with self.assertRaisesRegex(BaseException, 'add_wait: got cursor *'):
-            cs.add_wait(o)
+            cs.add_wait(ops)
 
     def test_add_fetch(self):
-        cs = CurrentStatement(cursor, None)
-        o = Ops('PARSE', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 8)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('PARSE', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 8)
         with self.assertRaisesRegex(BaseException, 'add_fetch: wrong op_type *'):
-            cs.add_fetch(o)
+            cs.add_fetch(ops)
 
-        o = Ops('FETCH', wrong_cursor, 'c=475,e=474,p=1,cr=4,cu=0,mis=0,r=10,dep=0,og=1,plh=2725028981,tim=5793512314300', fname, 2)
+        ops = Ops('FETCH', WRONG_CURSOR, 'c=475,e=474,p=1,cr=4,cu=0,mis=0,r=10,dep=0,og=1,plh=2725028981,tim=5793512314300', FNAME, 2)
         with self.assertRaisesRegex(BaseException, 'add_fetch: got cursor *'):
-            cs.add_fetch(o)
+            cs.add_fetch(ops)
 
     def test_add_close(self):
-        cs = CurrentStatement(cursor, None)
-        o = Ops('CLOSE', cursor, 'c=0,e=4,dep=0,type=3,tim=5793512315335', fname, 8)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('CLOSE', CURSOR, 'c=0,e=4,dep=0,type=3,tim=5793512315335', FNAME, 8)
 
-        cs.add_close(o)
+        cs.add_close(ops)
         #with self.assertRaisesRegex(BaseException, 'add_close: already set!'):
         #    cs.add_close(o)
-        cs.add_close(o)
+        cs.add_close(ops)
         self.assertEqual(cs.close.c, 0)
         self.assertEqual(cs.close.e, 8)
 
-        cs = CurrentStatement(cursor, None)
-        o = Ops('PARSE', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 7)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('PARSE', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 7)
         with self.assertRaisesRegex(BaseException, 'add_close: wrong op_type *'):
-            cs.add_close(o)
+            cs.add_close(ops)
 
-        o = Ops('CLOSE', wrong_cursor, 'c=0,e=4,dep=0,type=3,tim=5793512315335', fname, 9)
+        ops = Ops('CLOSE', WRONG_CURSOR, 'c=0,e=4,dep=0,type=3,tim=5793512315335', FNAME, 9)
         with self.assertRaisesRegex(BaseException, 'add_close: got cursor *'):
-            cs.add_close(o)
+            cs.add_close(ops)
     def test_add_stat(self):
-        cs = CurrentStatement(cursor, None)
-        o = Ops('STAT', cursor, "id=1 cnt=1 pid=0 pos=1 obj=89434 op='TABLE ACCESS BY INDEX ROWID CUSTOMER_SEGMENT (cr=5 pr=0 pw=0 str=1 time=173 us cost=4 size=103 card=1)'", fname, 8)
-        cs.add_stat(o)
+        cs = CurrentStatement(CURSOR, None)
+        ops = Ops('STAT', CURSOR, "id=1 cnt=1 pid=0 pos=1 obj=89434 op='TABLE ACCESS BY INDEX ROWID CUSTOMER_SEGMENT (cr=5 pr=0 pw=0 str=1 time=173 us cost=4 size=103 card=1)'", FNAME, 8)
+        cs.add_stat(ops)
         self.assertEqual(len(cs.stat), 1)
 
-        o = Ops('PARSE', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 7)
+        ops = Ops('PARSE', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 7)
         with self.assertRaisesRegex(BaseException, 'add_stat: wrong op_type *'):
-            cs.add_stat(o)
+            cs.add_stat(ops)
 
-        o = Ops('STAT', wrong_cursor, "id=1 cnt=1 pid=0 pos=1 obj=89434 op='TABLE ACCESS BY INDEX ROWID CUSTOMER_SEGMENT (cr=5 pr=0 pw=0 str=1 time=173 us cost=4 size=103 card=1)'", fname, 8)
+        ops = Ops('STAT', WRONG_CURSOR, "id=1 cnt=1 pid=0 pos=1 obj=89434 op='TABLE ACCESS BY INDEX ROWID CUSTOMER_SEGMENT (cr=5 pr=0 pw=0 str=1 time=173 us cost=4 size=103 card=1)'", FNAME, 8)
         with self.assertRaisesRegex(BaseException, 'add_stat: got cursor *'):
-            cs.add_stat(o)
+            cs.add_stat(ops)
 
     def test_dump_to_db(self):
-        cs = CurrentStatement(cursor, None)
+        cs = CurrentStatement(CURSOR, None)
         with self.assertRaisesRegex(BaseException, 'dump_to_db: database not set!'):
             cs.dump_to_db()
     def test_is_not_empty(self):
-        cs = CurrentStatement(cursor, None)
+        cs = CurrentStatement(CURSOR, None)
         self.assertFalse(cs.is_not_empty())
-        o = Ops('STAT', cursor, "id=1 cnt=1 pid=0 pos=1 obj=89434 op='TABLE ACCESS BY INDEX ROWID CUSTOMER_SEGMENT (cr=5 pr=0 pw=0 str=1 time=173 us cost=4 size=103 card=1)'", fname, 8)
-        cs.add_stat(o)
+        ops = Ops('STAT', CURSOR, "id=1 cnt=1 pid=0 pos=1 obj=89434 op='TABLE ACCESS BY INDEX ROWID CUSTOMER_SEGMENT (cr=5 pr=0 pw=0 str=1 time=173 us cost=4 size=103 card=1)'", FNAME, 8)
+        cs.add_stat(ops)
         self.assertTrue(cs.is_not_empty())
 
 if __name__ == '__main__':
