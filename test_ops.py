d99b964e665674627f43eb2d2ec07221a081abb9
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-07-03 18:22:38 +0000

    Deploy linter

--- test_ops.py
+++ test_ops.py
@@ -1,124 +1,122 @@
 import unittest
 from ops import Ops
 
-cursor = '#140641987987624'
-fname = 'file.trc'
+CURSOR = '#140641987987624'
+FNAME = 'file.trc'
 class TestOps(unittest.TestCase):
     def test_init(self):
-        o = Ops('EXEC', cursor, 'c=73,e=73,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', fname, 3)
-        self.assertEqual(o.op_type, 'EXEC')
-        self.assertEqual(o.cursor, cursor)
-        self.assertEqual(o.c, 73)
-        self.assertEqual(o.e, 73)
-        self.assertEqual(o.p, 0)
-        self.assertEqual(o.cr, 0)
-        self.assertEqual(o.cu, 0)
-        self.assertEqual(o.mis, 0)
-        self.assertEqual(o.r, 0)
-        self.assertEqual(o.dep, 0)
-        self.assertEqual(o.og, 1)
-        self.assertEqual(o.plh, 2725028981)
-        self.assertEqual(o.tim, 5793511830834)
+        ops = Ops('EXEC', CURSOR, 'c=73,e=73,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', FNAME, 3)
+        self.assertEqual(ops.op_type, 'EXEC')
+        self.assertEqual(ops.cursor, CURSOR)
+        self.assertEqual(ops.c, 73)
+        self.assertEqual(ops.e, 73)
+        self.assertEqual(ops.p, 0)
+        self.assertEqual(ops.cr, 0)
+        self.assertEqual(ops.cu, 0)
+        self.assertEqual(ops.mis, 0)
+        self.assertEqual(ops.r, 0)
+        self.assertEqual(ops.dep, 0)
+        self.assertEqual(ops.og, 1)
+        self.assertEqual(ops.plh, 2725028981)
+        self.assertEqual(ops.tim, 5793511830834)
 
     def test_init_close(self):
-        o = Ops('CLOSE', '#140641987987624', 'c=3,e=3,dep=0,type=1,tim=5793511831927', fname, 2)
-        self.assertEqual(o.op_type, 'CLOSE')
-        self.assertEqual(o.cursor, cursor)
-        self.assertEqual(o.c, 3)
-        self.assertEqual(o.e, 3)
-        self.assertEqual(o.dep, 0)
-        self.assertEqual(o.type, 1)
-        self.assertEqual(o.tim, 5793511831927)
+        ops = Ops('CLOSE', '#140641987987624', 'c=3,e=3,dep=0,type=1,tim=5793511831927', FNAME, 2)
+        self.assertEqual(ops.op_type, 'CLOSE')
+        self.assertEqual(ops.cursor, CURSOR)
+        self.assertEqual(ops.c, 3)
+        self.assertEqual(ops.e, 3)
+        self.assertEqual(ops.dep, 0)
+        self.assertEqual(ops.type, 1)
+        self.assertEqual(ops.tim, 5793511831927)
 
         # Missing attribute,but present in __slots__: __getattr__ returns 0 instead
-        self.assertEqual(o.cu, 0)
+        self.assertEqual(ops.cu, 0)
 
     def test_init_wait(self):
         # FIXME: space in the beginning of the param string
-        o = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 1 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", fname, 2)
-        self.assertEqual(o.op_type, 'WAIT')
-        self.assertEqual(o.cursor, cursor)
-        self.assertEqual(o.e, 1)
-        self.assertEqual(o.tim, 5793511831582)
-        self.assertEqual(o.name, 'SQL*Net message to client')
+        ops = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 1 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", FNAME, 2)
+        self.assertEqual(ops.op_type, 'WAIT')
+        self.assertEqual(ops.cursor, CURSOR)
+        self.assertEqual(ops.e, 1)
+        self.assertEqual(ops.tim, 5793511831582)
+        self.assertEqual(ops.name, 'SQL*Net message to client')
 
         # Missing attribute, not in __slots__
         with self.assertRaises(AttributeError):
-            self.assertEqual(o.og, 1)
+            self.assertEqual(ops.og, 1)
 
     def test_merge(self):
-        o1 = Ops('EXEC', cursor, 'c=73,e=73,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', fname, 2)
+        ops1 = Ops('EXEC', CURSOR, 'c=73,e=73,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', FNAME, 2)
 
-        o2 = Ops('EXEC', '#0', 'c=123,e=223,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', fname, 1)
+        ops2 = Ops('EXEC', '#0', 'c=123,e=223,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', FNAME, 1)
         with self.assertRaises(ValueError):
-            o3 = o2.merge(o1)
+            ops3 = ops2.merge(ops1)
 
-        o2 = Ops('EXEC', cursor, 'c=123,e=223,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', fname, 1)
-        o3 = o2.merge(o1)
-        self.assertEqual(o2.op_type, o3.op_type)
-        self.assertEqual(o3.c, 196)
-        self.assertEqual(o3.e, 296)
-        self.assertEqual(o3.tim, 0)
+        ops2 = Ops('EXEC', CURSOR, 'c=123,e=223,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', FNAME, 1)
+        ops3 = ops2.merge(ops1)
+        self.assertEqual(ops2.op_type, ops3.op_type)
+        self.assertEqual(ops3.c, 196)
+        self.assertEqual(ops3.e, 296)
+        self.assertEqual(ops3.tim, 0)
 
-        o3 = Ops('EXEC', cursor, 'c=123,e=223,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', fname, 1)
-        o4 = o3.merge([o1, o2])
-        self.assertEqual(o4.c, o1.c + o2.c + o3.c)
-        self.assertEqual(o4.e, o1.e + o2.e + o3.e)
+        ops3 = Ops('EXEC', CURSOR, 'c=123,e=223,p=0,cr=0,cu=0,mis=0,r=0,dep=0,og=1,plh=2725028981,tim=5793511830834', FNAME, 1)
+        ops4 = ops3.merge([ops1, ops2])
+        self.assertEqual(ops4.c, ops1.c + ops2.c + ops3.c)
+        self.assertEqual(ops4.e, ops1.e + ops2.e + ops3.e)
 
-        o5 = o3.merge(None)
     def test_to_list(self):
         sql_id = 'abc123'
-        o1 = Ops('EXEC', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 1)
-        l = o1.to_list(0, sql_id)
-        self.assertEqual(len(l), len(o1.__slots__) + 13)
-        self.assertEqual(l[0], 0)
-        self.assertEqual(l[1], sql_id)
-        self.assertEqual(l[2], cursor)
-        self.assertEqual(l[3], 'EXEC')
-        self.assertEqual(l[4], o1.c)
-        self.assertEqual(l[5], o1.e)
-        self.assertEqual(l[6], o1.p)
-        self.assertEqual(l[7], o1.cr)
-        self.assertEqual(l[8], o1.cu)
-        self.assertEqual(l[9], o1.mis)
-        self.assertEqual(l[10], o1.r)
-        self.assertEqual(l[11], o1.dep)
-        self.assertEqual(l[12], o1.og)
-        self.assertEqual(l[13], o1.plh)
-        self.assertEqual(l[14], o1.tim)
-        self.assertEqual(l[15], 0) # type will be 0
-        self.assertEqual(l[16], '') 
-        self.assertEqual(l[17], '')
-        self.assertEqual(l[18], fname)
-        self.assertEqual(l[19], 1)
+        ops = Ops('EXEC', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 1)
+        lst = ops.to_list(0, sql_id)
+        self.assertEqual(len(lst), len(ops.__slots__) + 13)
+        self.assertEqual(lst[0], 0)
+        self.assertEqual(lst[1], sql_id)
+        self.assertEqual(lst[2], CURSOR)
+        self.assertEqual(lst[3], 'EXEC')
+        self.assertEqual(lst[4], ops.c)
+        self.assertEqual(lst[5], ops.e)
+        self.assertEqual(lst[6], ops.p)
+        self.assertEqual(lst[7], ops.cr)
+        self.assertEqual(lst[8], ops.cu)
+        self.assertEqual(lst[9], ops.mis)
+        self.assertEqual(lst[10], ops.r)
+        self.assertEqual(lst[11], ops.dep)
+        self.assertEqual(lst[12], ops.og)
+        self.assertEqual(lst[13], ops.plh)
+        self.assertEqual(lst[14], ops.tim)
+        self.assertEqual(lst[15], 0) # type will be 0
+        self.assertEqual(lst[16], '')
+        self.assertEqual(lst[17], '')
+        self.assertEqual(lst[18], FNAME)
+        self.assertEqual(lst[19], 1)
         # ts2
-        self.assertEqual(l[20], None)
+        self.assertEqual(lst[20], None)
 
     def test_empty_wait(self):
-        o1 = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 1 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", fname, 3)
-        o2 = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 66 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", fname, 4)
-        o3 = o2.merge(o1)
-        self.assertEqual(o3.e, 67)
-        self.assertEqual(o3.c, 0)
+        ops1 = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 1 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", FNAME, 3)
+        ops2 = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 66 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", FNAME, 4)
+        ops3 = ops2.merge(ops1)
+        self.assertEqual(ops3.e, 67)
+        self.assertEqual(ops3.c, 0)
 
     def test_str(self):
-        o1 = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 1 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", fname, 2)
-        self.assertRegex(str(o1), '^#140641987987624: WAIT*')
+        ops = Ops('WAIT', '#140641987987624', " nam='SQL*Net message to client' ela= 1 driver id=675562835 #bytes=1 p3=0 obj#=89440 tim=5793511831582", FNAME, 2)
+        self.assertRegex(str(ops), '^#140641987987624: WAIT*')
 
-        o1 = Ops('EXEC', cursor, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', fname, 4)
-        self.assertRegex(str(o1), '^#140641987987624: EXEC*')
+        ops = Ops('EXEC', CURSOR, 'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 4)
+        self.assertRegex(str(ops), '^#140641987987624: EXEC*')
 
-        o1 = Ops('CLOSE', '#140641987987624', 'c=3,e=3,dep=0,type=1,tim=5793511831927', fname, 4)
-        self.assertRegex(str(o1), '^#140641987987624: CLOSE*')
+        ops = Ops('CLOSE', '#140641987987624', 'c=3,e=3,dep=0,type=1,tim=5793511831927', FNAME, 4)
+        self.assertRegex(str(ops), '^#140641987987624: CLOSE*')
 
-        o1 = Ops('STAR', None, '123.223', fname, 4, name='SESSION ID')
-        self.assertRegex(str(o1), '^\*\*\* SESSION ID*')
+        ops = Ops('STAR', None, '123.223', FNAME, 4, name='SESSION ID')
+        self.assertRegex(str(ops), r'^\*\*\* SESSION ID*')
 
-        o1 = Ops('PIC', cursor, "len=80 dep=0 uid=331 oct=3 lid=331 tim=7104844976089 hv=1167462720 ad='9d4125228' sqlid='6v48b7j2tc4a0'", fname, 4, name='select dummy from dual')
-        self.assertRegex(str(o1), '^PARSING IN CURSOR (.*) tim=7104844976089(.*)')
+        ops = Ops('PIC', CURSOR, "len=80 dep=0 uid=331 oct=3 lid=331 tim=7104844976089 hv=1167462720 ad='9d4125228' sqlid='6v48b7j2tc4a0'", FNAME, 4, name='select dummy from dual')
+        self.assertRegex(str(ops), '^PARSING IN CURSOR (.*) tim=7104844976089(.*)')
 
-        o1 = Ops('XCTEND', None, 'rlbk=0, rd_only=1, tim=5793512315347', fname, 4)
-        self.assertRegex(str(o1), '^XCTEND rlbk=0,*')
+        ops = Ops('XCTEND', None, 'rlbk=0, rd_only=1, tim=5793512315347', FNAME, 4)
+        self.assertRegex(str(ops), '^XCTEND rlbk=0,*')
 if __name__ == '__main__':
     unittest.main()
-
