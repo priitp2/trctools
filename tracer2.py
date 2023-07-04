9b4ad64571de40322be202dc4bc5609b5db1a36f
Priit Piipuu <priit.piipuu@gmail.com>
2023-07-04 14:51:24 +0000

    Deploy linter

--- tracer2.py
+++ tracer2.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3.11
 
 import argparse
-import re
-import sys
+import logging
+import time
 #from scipy.stats import shapiro,kstest
 from util import Filer
 from CallTracker import CallTracker
-import logging
-import time
 
 max_fetch_elapsed = 500000
 max_exec_elapsed = 500000
@@ -17,11 +15,16 @@ parser.add_argument('trace_files', metavar='files', type=str, nargs='+',
                             help='Trace files to process')
 parser.add_argument('--norm', type=bool, default = False, dest='norm',
                             help="Perform Shapiro-Wilk normality test on values")
-parser.add_argument('--db', type=str, default = None, dest='db', help="Persists raw data in the db, supported implementations: oracle, parquet")
-parser.add_argument('--dbdir', type=str, default = 'arrow', dest='dbdir', help="Directory for the parquet files")
-parser.add_argument('--db-file-prefix', type=str, default = 'parquet', dest='file_prefix', help="File name prefix")
-parser.add_argument('--logfile', type=str, default = None, dest='logfile', help="Sends output to the file")
-parser.add_argument('--log-level', type=str, default = None, dest='loglevel', help="Sets logging level: from DEBUG to CRITICAL")
+parser.add_argument('--db', type=str, default = None, dest='db',
+                    help="Persists raw data in the db, supported implementations: oracle, parquet")
+parser.add_argument('--dbdir', type=str, default = 'arrow', dest='dbdir',
+                    help="Directory for the parquet files")
+parser.add_argument('--db-file-prefix', type=str, default = 'parquet', dest='file_prefix',
+                    help="File name prefix")
+parser.add_argument('--logfile', type=str, default = None, dest='logfile',
+                    help="Sends output to the file")
+parser.add_argument('--log-level', type=str, default = None, dest='loglevel',
+                    help="Sets logging level: from DEBUG to CRITICAL")
 
 args = parser.parse_args()
 
@@ -30,8 +33,9 @@ log_level = logging.INFO
 if args.loglevel:
     log_level = getattr(logging, args.loglevel.upper(), None)
     if not isinstance(log_level, int):
-        raise ValueError('Invalid log level: %s' % loglevel)
-logging.basicConfig(filename = args.logfile, level = log_level, format = '%(asctime)s - %(name)s - %(levelname)s:%(message)s')
+        raise ValueError(f'Invalid log level: {log_level}')
+logging.basicConfig(filename = args.logfile, level = log_level,
+        format = '%(asctime)s - %(name)s - %(levelname)s:%(message)s')
 
 #if args.logfile:
 #    logging.basicConfig(args.logfile)
@@ -57,7 +61,7 @@ cumul_lines = 0
 cumul_time = 0
 filer = Filer()
 for fname in args.trace_files:
-    print("[{}/{}] processing file {}".format(fcount, no_files, fname))
+    print(f"[{fcount}/{no_files}] processing file {fname}")
     start = time.time_ns()
     lines = filer.process_file(tracker, fname)
     cumul_lines += lines
