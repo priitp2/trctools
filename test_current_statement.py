ebf5ae282e16c8c2ad0a2ce77742432fd091dcd7
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-07-06 16:18:56 +0000

    Implement add_ops() and is_set()

--- test_current_statement.py
+++ test_current_statement.py
@@ -6,10 +6,9 @@ CURSOR = '#123'
 WRONG_CURSOR = '#321'
 FNAME = 'trace.trc'
 class TestCurrentStatement(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.cstat = CurrentStatement(CURSOR, None)
-        cls.happy_ops = {
+    def setUp(self):
+        self.cstat = CurrentStatement(CURSOR, None)
+        self.happy_ops = {
                 'PARSE':Ops('PARSE', CURSOR, 
                     'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FNAME, 0),
                 'EXEC':Ops('EXEC', CURSOR,
@@ -117,7 +116,7 @@ class TestCurrentStatement(unittest.TestCase):
         ops = Ops('STAT', CURSOR, "id=1 cnt=1 pid=0 pos=1 obj=89434 op='TABLE ACCESS BY INDEX ROWID CUSTOMER_SEGMENT (cr=5 pr=0 pw=0 str=1 time=173 us cost=4 size=103 card=1)'", FNAME, 8)
         cs.add_stat(ops)
         self.assertTrue(cs.is_not_empty())
-    def test_add_opt(self):
+    def test_add_ops(self):
         for ops in self.happy_ops:
             self.cstat.add_ops(self.happy_ops[ops])
         stat = self.happy_ops['STAT']
@@ -135,18 +134,20 @@ class TestCurrentStatement(unittest.TestCase):
         self.assertFalse(self.cstat.is_set('PIC'))
 
         wrong_cs = Ops('WAIT', WRONG_CURSOR, " nam='db file sequential read' ela= 403 file#=414 block#=2682927 ", FNAME, 3)
-        with self.assertRaisesRegex(BaseException, 'add_ops: got cursor *'):
+        with self.assertRaisesRegex(BaseException, 'add_ops: wrong cursor *'):
             self.cstat.add_ops(wrong_cs)
 
-        wrong_ops = Ops('STAR', WRONG_CURSOR, " ", FNAME, 3) 
-        with self.assertRaisesRegex(BaseException, 'add_ops: unknown ops: *'):
+        wrong_ops = Ops('STAR', CURSOR, " ", FNAME, 3) 
+        with self.assertRaisesRegex(BaseException, 'add_ops: unknown ops type:*'):
             self.cstat.add_ops(wrong_ops)
 
         ops = self.happy_ops['EXEC']
         with self.assertRaisesRegex(BaseException, 'add_ops: already set: *'):
             self.cstat.add_ops(ops)
-    def test_opt_is_set(self):
-        self.fail('Not implemented')
+
+    def test_is_set(self):
+        print(self.cstat.ops)
+        self.assertFalse(self.cstat.is_set('EXEC'))
 
 if __name__ == '__main__':
     unittest.main()
