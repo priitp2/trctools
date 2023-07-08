18bf861ed1ba53a51b96307b9de72fca1580141d
Priit Piipuu <priit.piipuu@gmail.com>
2023-07-08 09:24:37 +0000

    Module test -> test_constants

--- test_current_statement.py
+++ test_current_statement.py
@@ -2,36 +2,36 @@ import unittest
 from current_statement import CurrentStatement
 from ops import Ops
 from test_db import DB
-import test
+import test_constants
 
 class TestCurrentStatement(unittest.TestCase):
     def setUp(self):
-        self.cstat = CurrentStatement(test.CURSOR, None)
+        self.cstat = CurrentStatement(test_constants.CURSOR, None)
     def test_dump_to_db(self):
         with self.assertRaisesRegex(BaseException, 'dump_to_db: database not set!'):
             self.cstat.dump_to_db()
 
         self.cstat.dbs = DB()
-        for ops in test.CORRECT_OPS:
-            self.cstat.add_ops(test.CORRECT_OPS[ops])
+        for ops in test_constants.CORRECT_OPS:
+            self.cstat.add_ops(test_constants.CORRECT_OPS[ops])
         self.cstat.dump_to_db()
-        self.assertEqual(len(self.cstat.dbs.batches), len(test.CORRECT_OPS))
+        self.assertEqual(len(self.cstat.dbs.batches), len(test_constants.CORRECT_OPS))
     def test_is_not_empty(self):
         self.assertFalse(self.cstat.is_not_empty())
-        self.cstat.add_ops(test.CORRECT_OPS['EXEC'])
+        self.cstat.add_ops(test_constants.CORRECT_OPS['EXEC'])
         self.assertTrue(self.cstat.is_not_empty())
 
-        self.cstat = CurrentStatement(test.CURSOR, None)
-        wait = test.CORRECT_OPS['WAIT']
+        self.cstat = CurrentStatement(test_constants.CURSOR, None)
+        wait = test_constants.CORRECT_OPS['WAIT']
         for i in range(0, 3):
             self.cstat.add_ops(wait)
         self.assertTrue(self.cstat.is_not_empty())
     def test_add_ops(self):
-        for ops in test.CORRECT_OPS:
-            self.cstat.add_ops(test.CORRECT_OPS[ops])
-        stat = test.CORRECT_OPS['STAT']
+        for ops in test_constants.CORRECT_OPS:
+            self.cstat.add_ops(test_constants.CORRECT_OPS[ops])
+        stat = test_constants.CORRECT_OPS['STAT']
         self.cstat.add_ops(stat)
-        wait = test.CORRECT_OPS['WAIT']
+        wait = test_constants.CORRECT_OPS['WAIT']
         self.cstat.add_ops(wait)
 
         self.assertTrue(self.cstat.is_set('PARSE'))
@@ -43,15 +43,15 @@ class TestCurrentStatement(unittest.TestCase):
         self.assertFalse(self.cstat.is_set('BINDS'))
         self.assertFalse(self.cstat.is_set('PIC'))
 
-        wrong_cs = Ops('WAIT', test.WRONG_CURSOR, " nam='db file sequential read' ela= 403 file#=414 block#=2682927 ", test.FNAME, 3)
+        wrong_cs = Ops('WAIT', test_constants.WRONG_CURSOR, " nam='db file sequential read' ela= 403 file#=414 block#=2682927 ", test_constants.FNAME, 3)
         with self.assertRaisesRegex(BaseException, 'add_ops: wrong cursor *'):
             self.cstat.add_ops(wrong_cs)
 
-        wrong_ops = Ops('STAR', test.CURSOR, " ", test.FNAME, 3) 
+        wrong_ops = Ops('STAR', test_constants.CURSOR, " ", test_constants.FNAME, 3) 
         with self.assertRaisesRegex(BaseException, 'add_ops: unknown ops type:*'):
             self.cstat.add_ops(wrong_ops)
 
-        ops = test.CORRECT_OPS['EXEC']
+        ops = test_constants.CORRECT_OPS['EXEC']
         with self.assertRaisesRegex(BaseException, 'add_ops: already set: *'):
             self.cstat.add_ops(ops)
 
@@ -61,11 +61,11 @@ class TestCurrentStatement(unittest.TestCase):
     def test_count_ops(self):
         self.assertEqual(self.cstat.count_ops('FETCH'), 0)
 
-        ops = test.CORRECT_OPS['EXEC']
+        ops = test_constants.CORRECT_OPS['EXEC']
         self.cstat.add_ops(ops)
         self.assertEqual(self.cstat.count_ops('EXEC'), 1)
 
-        wait = test.CORRECT_OPS['WAIT']
+        wait = test_constants.CORRECT_OPS['WAIT']
         for i in range(0, 3):
             self.cstat.add_ops(wait)
         self.assertEqual(self.cstat.count_ops('WAIT'), 3)
