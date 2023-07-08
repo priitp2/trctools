8143e288c7c83e0bf87f8c7386eb499163e81678
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-07-08 12:17:49 +0000

    Deploy linter. Remove unneeded method stubs

--- test_db.py
+++ test_db.py
@@ -1,12 +1,8 @@
 class DB:
+    """Mock db for the tests"""
     def __init__(self):
         self.exec_id = 0
         self.batches = []
-        self.cursor_statement_batches = []
-    def add_rows(self, sql_id, rt):
-        pass
-    def add_cursor(self, c):
-        pass
     def get_exec_id(self):
         self.exec_id += 1
         return self.exec_id
@@ -14,8 +10,5 @@ class DB:
         if len(ops) == 0:
             return
         self.batches.append(ops)
-    def insert_cursors(self, cs):
-        self.cursor_statement_batches.append(ops)
     def flush(self):
         pass
-
