5989c891e64d7fcfdf36f64b03958e4d8300fb71
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-07-08 09:23:57 +0000

    Deploy linter

--- summary.py
+++ summary.py
@@ -5,16 +5,16 @@ from hdrh.histogram import HdrHistogram
 class SummaryDuckdb:
     def __init__(self, dbdir):
         self.dbdir = dbdir
-        d.sql("""create or replace view elapsed_time as
+        d.sql(f"""create or replace view elapsed_time as
                 select sql_id,
                     exec_id,
                     last( ts order by ts) - first(ts order by ts) as ela
                 from
-                    read_parquet('{}')
+                    read_parquet('{dbdir}')
                 where
                     ts is not null
                 group by sql_id, exec_id;
-              """.format(dbdir))
+              """)
 
     def summary(self):
         res = d.sql(f"""
@@ -55,14 +55,14 @@ class SummaryDuckdb:
         print(res)
 
     def create_hdrh(self, sql_id):
-        r = d.sql("select ela from elapsed_time where sql_id = '{}'".format(sql_id)).fetchall()
+        r = d.sql(f"select ela from elapsed_time where sql_id = '{sql_id}'").fetchall()
         resp_hist = HdrHistogram(1, 1000000000, 1)
-        for e in r:
-            resp_hist.record_value(e[0])
-        with open("elapsed_{}.out".format(sql_id), 'wb') as f:
+        for ela in r:
+            resp_hist.record_value(ela[0])
+        with open(f"elapsed_{sql_id}.out", 'wb') as f:
             resp_hist.output_percentile_distribution(f, 1.0)
     def outliers(self, sql_id, thresold):
-        res = d.sql("""select cursor_id "cursor",
+        res = d.sql(f"""select cursor_id "cursor",
                             exec_id,
                             ops,
                             elapsed_time,
@@ -72,60 +72,65 @@ class SummaryDuckdb:
                             file_name,
                             line
                         from
-                            read_parquet('{}')
+                            read_parquet('{self.dbdir}')
                         where
-                            sql_id = '{}'
-                            and exec_id in (select exec_id from elapsed_time where sql_id = '{}' and ela > {})
+                            sql_id = '{sql_id}'
+                            and exec_id in (select exec_id from elapsed_time where sql_id = '{sql_id}' and ela > {thresold})
                         order by exec_id, ts
-                    """.format(self.dbdir, sql_id, sql_id, thresold))
+                    """)
         print(res)
     def waits(self, sql_id):
-        res = d.sql("""select wait_name wait,
+        pred = ''
+        if sql_id:
+            pred = f"sql_id = '{sql_id}' and"
+        res = d.sql(f"""select wait_name wait,
                             count(*) count,
                             sum(elapsed_time) sum,
                             median(elapsed_time) median,
                             percentile_disc(0.99) within group(order by elapsed_time) "99th percentile",
                             max(elapsed_time) max
-                        from read_parquet('{}')
-                        where sql_id = '{}'
-                            and wait_name <> ''
+                        from read_parquet('{self.dbdir}')
+                        where {pred}
+                             wait_name <> '' and ops = 'WAIT'
                         group by wait_name order by count(*) desc
-                    """.format(self.dbdir, sql_id))
+                    """)
         print(res)
-#        for r in res:
-#            print(r)
     def wait_histogram(self, wait_name, fname):
-        res = d.sql("""select elapsed_time
+        res = d.sql(f"""select elapsed_time
                         from
-                            read_parquet('{}')
+                            read_parquet('{self.dbdir}')
                         where
                             ops = 'WAIT'
-                            and wait_name = '{}'
-                    """.format(self.dbdir, wait_name)).fetchall()
+                            and wait_name = '{wait_name}'
+                    """).fetchall()
         resp_hist = HdrHistogram(1, 1000000000, 1)
-        for w in res:
-            resp_hist.record_value(w[0])
+        for ela in res:
+            resp_hist.record_value(ela[0])
         with open(fname, 'wb') as f:
             resp_hist.output_percentile_distribution(f, 1.0)
     def db(self):
-        res = d.sql("""select count(*), count(distinct file_name), count(distinct sql_id), count(distinct cursor_id)
-                        from read_parquet('{}')
-                    """.format(self.dbdir))
+        res = d.sql(f"""select count(*) "rows",
+                            count(distinct file_name) files,
+                            count(distinct sql_id) "sql_id's",
+                            count(distinct cursor_id) cursors
+                        from read_parquet('{self.dbdir}')
+                    """)
         print(res)
 
-        res = d.sql("""select file_name, count(*) "rows", min(ts2) "first timestamp"
-                        from read_parquet('{}')
+        res = d.sql(f"""select file_name, count(*) "rows", min(ts2) "first timestamp"
+                        from read_parquet('{self.dbdir}')
                         group by file_name order by count(*)
-                    """.format(self.dbdir))
+                    """)
         print(res)
 
 parser = argparse.ArgumentParser(description='Generate summary from processed traces')
-parser.add_argument('action', type=str, choices=['summary', 'histogram', 'outliers', 'waits', 'wait_histogram', 'db'],
-                                    help='Directory for Parquet files')
+parser.add_argument('action', type=str, choices=['summary', 'histogram', 'outliers', 'waits',
+                                                    'wait_histogram', 'db'],
+                     help='Directory for Parquet files')
 parser.add_argument('--sql_id', type=str, dest='sql_id',
-                                    help="Comma separated list of sql_id's for which summary is produced")
+                     help="Comma separated list of sql_id's for which summary is produced")
 parser.add_argument('--thresold', type=str, dest='thresold',
-                                    help="Thresold in microsecond for which the outliers are displayed")
+                     help="Thresold in microsecond for which the outliers are displayed")
 parser.add_argument('--dbdir', metavar='dbdir', type=str,
                                     help='Directory for Parquet files')
 parser.add_argument('--wait_name', dest='wait_name', type=str,
@@ -142,10 +147,10 @@ elif args.action == 'histogram':
     for sqlid in args.sql_id.split(','):
         s.create_hdrh(args.sql_id)
 elif args.action == 'outliers':
-        s.outliers(args.sql_id, args.thresold)
+    s.outliers(args.sql_id, args.thresold)
 elif args.action == 'waits':
-        s.waits(args.sql_id)
+    s.waits(args.sql_id)
 elif args.action == 'wait_histogram':
-        s.wait_histogram(args.wait_name, args.fname)
+    s.wait_histogram(args.wait_name, args.fname)
 elif args.action == 'db':
-        s.db()
+    s.db()
