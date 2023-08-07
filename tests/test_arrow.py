28e1307f27f1ba9fdcad773ae9bba8139070408c
Priit Piipuu <priit.piipuu@gmail.com>
2023-08-07 16:16:45 +0000

    Cosmetics

--- tests/test_arrow.py
+++ tests/test_arrow.py
@@ -7,6 +7,7 @@ from call_tracker import CallTracker
 
 class TestArrow(unittest.TestCase):
     def test_lobs(self):
+        """ Checks that we have correct number of rows in the parquet file."""
         with tempfile.TemporaryDirectory() as db_dir:
             dbs = DB(db_dir, 'unittest')
             tracker = CallTracker(dbs)
@@ -14,13 +15,12 @@ class TestArrow(unittest.TestCase):
             filer.process_file(tracker, 'tests/traces/lobs.trc')
             tracker.flush()
 
-            res = d.sql("select ops, event_name, event_raw, file_name, line from "
-                        + f"read_parquet('{db_dir}/*') order by line;")
-            print(res)
             res = d.sql(f"select count(*) from read_parquet('{db_dir}/*') where ops like 'LOB%';")
             self.assertEqual(res.fetchone()[0], 14)
+
             res = d.sql(f"select count(*) from read_parquet('{db_dir}/*') where ops = 'WAIT';")
             self.assertEqual(res.fetchone()[0], 26)
+
             res = d.sql(f"select count(*) from read_parquet('{db_dir}/*');")
             self.assertEqual(res.fetchone()[0], 66)
 if __name__ == '__main__':
