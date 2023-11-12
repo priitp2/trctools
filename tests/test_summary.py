96023d0d1c4e0576d92f8b4455fe8fbf6b490f12
Priit Piipuu <priit.piipuu@kindredgroup.com>
2023-11-12 09:14:57 +0000

    Save filters in dict. Add filtering by CLIENT ID

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -3,9 +3,18 @@ import datetime
 import summary
 
 class TestSummary(unittest.TestCase):
-    def test_create_time_predicate(self):
-        pred = summary.create_time_predicate(None, None)
-        self.assertEqual(pred, '')
+    def test_create_preds(self):
+        filters = {}
+        preds = summary.create_preds(filters)
+        self.assertEqual(preds, '')
+
+        filters['start'] = ''
+        preds = summary.create_preds(filters)
+        self.assertRegex(preds, 'ts >= TIMESTAMP')
+
+        filters['client_id'] = 'superdry'
+        preds = summary.create_preds(filters)
+        self.assertRegex(preds, "client_id = 'superdry'")
 
 if __name__ == '__main__':
     unittest.main()
