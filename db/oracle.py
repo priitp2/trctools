3c3f17db69a6499a77f259b4c1a679382abd977e
Priit Piipuu <priit.piipuu@gmail.com>
2024-04-29 13:41:20 +0000

    Add error_code

--- db/oracle.py
+++ db/oracle.py
@@ -31,7 +31,7 @@ class DB:
             return
         cursor = self.connection.cursor()
         try:
-            cursor.executemany("insert into dbcall(exec_id, sql_id, cursor_id, ops, cpu_time, elapsed_time, ph_reads, cr_reads, current_reads, cursor_missed, rows_processed, rec_call_dp, opt_goal, plh, tim, c_type, wait_name, wait_raw, file_name, line, ts, len, pic_uid, oct, lid, hv, ad, rlbk, rd_only, lobtype, bytes) values(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, substr(:18, 0, 4000), :19, :20, :21, :22, :23, :24, :25, :26, :27, :28, :29, :30, :31)", self.batches)
+            cursor.executemany("insert into dbcall(exec_id, sql_id, cursor_id, ops, cpu_time, elapsed_time, ph_reads, cr_reads, current_reads, cursor_missed, rows_processed, rec_call_dp, opt_goal, plh, tim, c_type, wait_name, wait_raw, file_name, line, ts, len, pic_uid, oct, lid, hv, ad, rlbk, rd_only, lobtype, bytes) values(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, substr(:18, 0, 4000), :19, :20, :21, :22, :23, :24, :25, :26, :27, :28, :29, :30, :31, :32)", self.batches)
         except Exception as e:
             print(self.batches)
             print(e)
