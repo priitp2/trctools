eb014bddb840e2d4ec4c885c30ada23b3c8abf0d
Priit Piipuu <priit.piipuu@gmail.com>
2025-05-30 21:35:21 +0200

    Sync calling conventions for PIC and HEADER with the rest of the code

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -13,7 +13,7 @@ FMETA['FILE_NAME'] = 'trace.trc'
 FMETA['LINE_COUNT'] = 77
 TRACKED_OPS = {
     'PIC':ops.ops_factory('PIC', CURSOR, "len=80 dep=0 uid=331 oct=3 lid=331 tim=7104844976089 hv=1167462720 " \
-        + "ad='9d4125228' sqlid='6v48b7j2tc4a0'", FMETA, TS_CALLBACK, name='select dummy from dual'),
+        + "ad='9d4125228' sqlid='6v48b7j2tc4a0'", FMETA, TS_CALLBACK),
     'PARSE':ops.ops_factory('PARSE', CURSOR,
         'c=73,e=73,p=1,cr=2,cu=3,mis=4,r=5,dep=6,og=7,plh=2725028981,tim=5793511830834', FMETA,
         TS_CALLBACK),
@@ -32,7 +32,7 @@ TRACKED_OPS = {
 UNTRACKED_OPS = {
         'STAR':ops.ops_factory('STAR', None, 'jdbcthin : 21.5.0.0.0', FMETA, None, 'CLIENT DRIVER', datetime.datetime.today()),
     'XCTEND':ops.ops_factory('XCTEND', None, 'rlbk=0, rd_only=1, tim=5793512315347', FMETA, TS_CALLBACK),
-    'HEADER':ops.ops_factory('HEADER', None, 'Build label', FMETA, 'RDBMS_19.16.0.0.0DBRUR_LINUX.X64_230111'),
+    'HEADER':ops.ops_factory('HEADER', None, 'Build label', FMETA, lambda x: None, 'RDBMS_19.16.0.0.0DBRUR_LINUX.X64_230111', None),
     'LOB':ops.ops_factory('LOBWRITE', None, "type=TEMPORARY LOB,bytes=5,c=169,e=169,p=0,cr=0,cu=7,tim=4696599871319", FMETA,
         TS_CALLBACK)
 }
